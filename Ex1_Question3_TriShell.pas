f5fe46aea3993e36849278e7cf06f1859fc1354f
Communauté des Enseignants d'Informatique en Tunisie <40127534+GroupeInfoTun@users.noreply.github.com>
2018-06-12 10:40:02 +0200

    Update Ex1_Question3_TriShell.pas

--- Ex1_Question3_TriShell.pas
+++ Ex1_Question3_TriShell.pas
@@ -1,3 +1,4 @@
+
 (*
 Un programme Pascal qui génère aléatoirement 100 Tableaux d'entiers
 de 2 chiffres, la taille de chacun des tableaux est générée aussi
@@ -22,20 +23,19 @@ Uses Wincrt, Shellapi;
 Type
   Tab = Array[1..50] Of Byte;
 Var
-	FTemp, FRap:Text;
+  FTemp, FRap: Text;
   Ti, Tr: Tab;
-	Ligne:String;
+  Ligne: String;
   NbDec_V1, NbDec_V2, NbDec_V3, N, i, NbDecTemp: Integer;
 {===================================================}
-
 Function Tri_Shell_V1(Var T:Tab; N:Byte): Byte;
 Var
   Kes, i, j, P, NombreDeDecalage : Integer;
 Begin
   NombreDeDecalage := 0;
   P := 0;
-  While (P < N) Do P := 3 + P;
-
+  While (P < N) Do
+    P := 3 + P;
   While (P <> 1) Do
     Begin
       P := (P - 3) + Ord(P - 3 = 0);
@@ -55,14 +55,15 @@ Begin
   Tri_Shell_V1 := NombreDeDecalage;
 End;
 {===================================================}
+
 Function Tri_Shell_V2(Var T:Tab; N:Byte): Byte;
 Var
   Kes, i, j, P, NombreDeDecalage : Integer;
 Begin
   NombreDeDecalage := 0;
   P := 1;
-  While (P < N) Do P := 2 * P;
-
+  While (P < N) Do
+    P := 2 * P;
   While (P <> 1) Do
     Begin
       P := (P Div 2);
@@ -82,14 +83,15 @@ Begin
   Tri_Shell_V2 := NombreDeDecalage;
 End;
 {===================================================}
+
 Function Tri_Shell_V3(Var T:Tab; N:Byte): Byte;
 Var
   Kes, i, j, P, NombreDeDecalage : Integer;
 Begin
   NombreDeDecalage := 0;
   P := 1;
-  While (P < N) Do P := 3 * P + 1;
-
+  While (P < N) Do
+    P := 3 * P + 1;
   While (P <> 1) Do
     Begin
       P := (P Div 3);
@@ -109,77 +111,78 @@ Begin
   Tri_Shell_V3 := NombreDeDecalage;
 End;
 {===================================================}
-
 Procedure Remplir(Var T:Tab; N:Integer);
 Var
   i: Integer;
 Begin
-  For i:=1 To N Do T[i] := 10+Random(90);
+  For i:=1 To N Do
+    T[i] := 10+Random(90);
 End;
 {===================================================}
+
 Procedure TableauVersFichier(Var F:Text; T:Tab; N:Integer);
 Var
   i: Integer;
 Begin
-	For i:=1 To N Do Write(F, T[i]:3);
-	Writeln(F);
+  For i:=1 To N Do
+    Write(F, T[i]:3);
+  Writeln(F);
 End;
 {===================================================}
 Begin
-	Randomize;
-	Assign(FTemp, 'Tmp.txt');
-	ReWrite(FTemp);
+  Randomize;
+  Assign(FTemp, 'Tmp.txt');
+  Rewrite(FTemp);
 {--Tri de 100 Tableaux   --------}
-	NbDec_V1:=0;
-	NbDec_V2:=0;
-	NbDec_V3:=0;
-	For i:=1 To 100 Do
-	Begin
-		  N := 5+Random(46);
-			Writeln(FTemp, ' __ N=', N,' __________');
-		  Remplir(Ti, N);
-			TableauVersFichier(FTemp, Ti, N);
-			Write(FTemp, #10' Nb de decalages=  ');
-		{--}
-		  Tr := Ti;
-			NbDecTemp:=Tri_Shell_V1(Tr, N);
-			NbDec_V1:=NbDec_V1+ NbDecTemp;
-			Write(FTemp, 'Suite-1: ', NbDecTemp,' | ');
-		{--}
-		  Tr := Ti;
-			NbDecTemp:=Tri_Shell_V2(Tr, N);
-			NbDec_V2:=NbDec_V2+ NbDecTemp;
-			Write(FTemp, 'Suite-2: ', NbDecTemp,' | ');
-		{--}
-		  Tr := Ti;
-			NbDecTemp:=Tri_Shell_V3(Tr, N);
-			NbDec_V3:=NbDec_V3+ NbDecTemp;
-			Writeln(FTemp, 'Suite-3: ', NbDecTemp);
-		{--}
-			Writeln(FTemp, '___________________________________________________________________________');
-	End;
+  NbDec_V1 := 0;
+  NbDec_V2 := 0;
+  NbDec_V3 := 0;
+  For i:=1 To 100 Do
+    Begin
+      N := 5+Random(46);
+      Writeln(FTemp, ' __ N=', N,' __________');
+      Remplir(Ti, N);
+      TableauVersFichier(FTemp, Ti, N);
+      Write(FTemp, #10' Nb de decalages=  ');
+ {--}
+      Tr := Ti;
+      NbDecTemp := Tri_Shell_V1(Tr, N);
+      NbDec_V1 := NbDec_V1+ NbDecTemp;
+      Write(FTemp, 'Suite-1: ', NbDecTemp,' | ');
+ {--}
+      Tr := Ti;
+      NbDecTemp := Tri_Shell_V2(Tr, N);
+      NbDec_V2 := NbDec_V2+ NbDecTemp;
+      Write(FTemp, 'Suite-2: ', NbDecTemp,' | ');
+ {--}
+      Tr := Ti;
+      NbDecTemp := Tri_Shell_V3(Tr, N);
+      NbDec_V3 := NbDec_V3+ NbDecTemp;
+      Writeln(FTemp, 'Suite-3: ', NbDecTemp);
+ {--}
+      Writeln(FTemp, '___________________________________________________________________________');
+    End;
 {--Génération du Rapport --------}
-	Assign(FRap, 'Rapport.txt');
-	ReWrite(FRap);
-	Reset(FTemp);
-	Writeln(FRap, '___________________________________________________');
-	Writeln(FRap, '  Tri Shell de 100 Tableaux generes aleatoirement');
-	Writeln(FRap, '___________________________________________________'#10);
-	Writeln(FRap, 'Le Nombre Total de Decalages pour : '#10);
-	Writeln(FRap, 'La Suite-1: P=0, P=3+P   === ', NbDec_V1,#10);
-	Writeln(FRap, 'La Suite-2: P=1, P=2*P   === ', NbDec_V2,#10);
-	Writeln(FRap, 'La Suite-3: P=1, P=3*P+1 === ', NbDec_V3,#10);
-	Writeln(FRap, '___________________________________________________________________________');
-	Writeln(FRap, 'Les Tableaux');
-	Writeln(FRap, '___________________________________________________________________________');
-
-	While not EOF(FTemp) Do
-	Begin
-			Readln(FTemp, Ligne);
-			Writeln(FRap, Ligne);
-	End;
-	Close(FRap);
-	Close(FTemp);
-	Erase(FTemp);
-	ShellExecute(0,PChar('open'),PChar('Rapport.txt'), '','',3);
+  Assign(FRap, 'Rapport.txt');
+  Rewrite(FRap);
+  Reset(FTemp);
+  Writeln(FRap, '___________________________________________________');
+  Writeln(FRap, '  Tri Shell de 100 Tableaux generes aleatoirement');
+  Writeln(FRap, '___________________________________________________'#10);
+  Writeln(FRap, 'Le Nombre Total de Decalages pour : '#10);
+  Writeln(FRap, 'La Suite-1: P=0, P=3+P   === ', NbDec_V1,#10);
+  Writeln(FRap, 'La Suite-2: P=1, P=2*P   === ', NbDec_V2,#10);
+  Writeln(FRap, 'La Suite-3: P=1, P=3*P+1 === ', NbDec_V3,#10);
+  Writeln(FRap, '___________________________________________________________________________');
+  Writeln(FRap, 'Les Tableaux');
+  Writeln(FRap, '___________________________________________________________________________');
+  While Not Eof(FTemp) Do
+    Begin
+      Readln(FTemp, Ligne);
+      Writeln(FRap, Ligne);
+    End;
+  Close(FRap);
+  Close(FTemp);
+  Erase(FTemp);
+  ShellExecute(0,PChar('open'),PChar('Rapport.txt'), '','',3);
 End.
